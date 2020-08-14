eb5b9d6581f6c0110a796ea43ca47b1c554a7911
Logan Graham <omacranger@gmail.com>
2020-08-14 08:07:40 -0400

    - Initial commit for TypeScript refactor - Remove required dependencies to allow using any version of any package type (free or pro)

---
+++ src/index.ts
@@ -0,0 +1,118 @@
+/**
+ * Author: Logan Graham <loganparkergraham@gmail.com>
+ */
+
+import { readFileSync, existsSync, writeFileSync } from "fs";
+import { resolve } from "path";
+import { sync as makeDirSync } from "mkdirp";
+import { FontAwesomeOptions, Subset, SubsetOption } from "./types";
+
+const svg2ttf = require("svg2ttf"),
+    ttf2eot = require("ttf2eot"),
+    ttf2woff = require("ttf2woff"),
+    ttf2woff2 = require("ttf2woff2");
+
+/**
+ * Returns a list of all glyph names that don't exist in user-provided 'icons' parameter
+ *
+ * @param svgFile The contents of the SVG file we're looking at.
+ * @param fontFamily The current font family we're parsing.
+ * @param icons Array of glyph names we want to keep / output.
+ *
+ * @return string[] List of glyph names to remove from the output.
+ */
+function findGlyphsToRemove(svgFile: string, fontFamily: Subset, icons: string[]): string[] {
+    let glyphs = [],
+        matcher = new RegExp('<glyph glyph-name="([^"]+)"', "gms");
+
+    let current_match;
+
+    while ((current_match = matcher.exec(svgFile))) {
+        if (fontFamily === "duotone") {
+            // If we're matching duotone we need to remove the trailing `-secondary` or `-primary`
+            if (icons.indexOf(current_match[1].substring(0, current_match[1].lastIndexOf("-"))) === -1) {
+                glyphs.push(current_match[1]);
+            }
+        } else {
+            if (icons.indexOf(current_match[1]) === -1) {
+                glyphs.push(current_match[1]);
+            }
+        }
+    }
+
+    return glyphs;
+}
+
+/**
+ * This function will take an object of glyph names and output a subset of the standard fonts optimized in size for
+ * use on websites / external resources.
+ *
+ * @param subset Array or Object containing glyph / font family names.
+ * @param outputDir Directory output generated webfonts.
+ * @param options Object of options / tweaks for further customization. Defaults to 'Free' package.
+ */
+function fontawesomeSubset(subset: SubsetOption, outputDir: string, options: FontAwesomeOptions = {}) {
+    const opts = {
+        package: "free",
+        ...options
+    };
+
+    // Maps style to actual font name / file name.
+    const fontMap: Record<Subset, string> = {
+        solid: "fa-solid-900",
+        light: "fa-light-300",
+        regular: "fa-regular-400",
+        brands: "fa-brands-400",
+        duotone: "fa-duotone-900"
+    };
+    const fontTypes = Object.keys(fontMap);
+
+    // If 'subset' is set to array, turn into object defaulted for 'solid' use (fontawesome free)
+    if (Array.isArray(subset)) {
+        subset = { solid: subset };
+    }
+
+    const entries = Object.entries(subset);
+    for (const [key, icons] of entries) {
+        // Skip if current font family is not found in font_map.
+        if (fontTypes.indexOf(key) === -1) {
+            continue;
+        }
+
+        // Bail early if icons isn't set, or isn't an array
+        if (!Array.isArray(icons)) {
+            continue;
+        }
+
+        const fontFamily = key as keyof typeof fontMap;
+        const svgFileName = fontMap[fontFamily];
+        const svgFilePath = `node_modules/@fortawesome/fontawesome-${opts.package}/webfonts/${svgFileName}.svg`;
+
+        if (!existsSync(svgFilePath)) {
+            console.error("Unable to find SVG file. Could be missing fontawesome dependencies. Make sure you have your preferred FontAwesome package in package.json and try running `npm install` or changing the font style.");
+            continue;
+        }
+
+        const svgFile = readFileSync(svgFilePath).toString();
+        const glyphsToRemove = findGlyphsToRemove(svgFile, fontFamily, icons);
+        const svgContentsNew = svgFile.replace(new RegExp(`(<glyph glyph-name="(${glyphsToRemove.join("|")})".*?\\/>)`, "gms"), "").replace(/>\s+</gms, "><");
+        const ttfUtils = svg2ttf(svgContentsNew, {
+            fullname: "FontAwesome " + fontFamily,
+            familyname: "FontAwesome",
+            subfamilyname: fontFamily
+        });
+        const ttf = Buffer.from(ttfUtils.buffer);
+
+        makeDirSync(resolve(outputDir));
+
+        const outputFile = resolve(outputDir, svgFileName);
+
+        writeFileSync(`${outputFile}.svg`, svgContentsNew);
+        writeFileSync(`${outputFile}.ttf`, ttf);
+        writeFileSync(`${outputFile}.eot`, ttf2eot(ttf).buffer);
+        writeFileSync(`${outputFile}.woff`, ttf2woff(ttf).buffer);
+        writeFileSync(`${outputFile}.woff2`, ttf2woff2(ttf));
+    }
+}
+
+export = fontawesomeSubset;
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface FontAwesomeOptions {
+    package?: "free" | "pro";
+}
+
+export type Subset = "solid" | "light" | "regular" | "brands" | "duotone";
+
+export type GlyphName = string;
+
+export type SubsetOption = GlyphName[] | Partial<Record<Subset, GlyphName[]>>;
